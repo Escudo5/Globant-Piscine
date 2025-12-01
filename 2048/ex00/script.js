b1a4b2004ce98c3844e2cbbbfca094443ce68997
Escudo5 <sergiomargar03@gmail.com>
2025-12-01 16:01:44 +0100

    ya se inicia el tablero, se coloca un numero en un hueco libre random.

--- 2048/ex00/script.js
+++ 2048/ex00/script.js
@@ -1,4 +1,83 @@
-let grid = document.querySelector('.grid-container');
-let score = document.getElementById('score');
+let board = [[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]];
 
-on_load()
+function initBoard()
+{
+    board = [
+        [2, 0, 0, 0],
+        [0, 4, 0, 0],
+        [0, 0, 8, 0],
+        [0, 0, 0, 16]
+    ];
+    // for (let row = 0; row < 4; row++)
+    // {
+    //     board[row] = []; //creo un array vacio para la fila
+    //     for (let col = 0; col < 4; col++)
+    //     {
+    //         board[row][col] = 0;
+    //     }
+    // }
+    console.log("Tablero iniciado", board);
+}
+
+//inicia el tablero todo a 0 o con numeros de prueba, lo imprime por consola.
+window.onload = function()
+{
+    initBoard();
+    console.log("window loaded");
+    addRandomTile();
+    renderBoard();
+    let score = document.getElementById('score');
+    console.log(score);
+}
+
+function renderBoard()
+{
+    let grid = document.querySelector('.grid-container');
+    grid.innerHTML = ''; // limpia todo el contenido del grid. Para teenr siempre 16 celdas. Si no habria cada vez mas celdas.
+    //recorrer tablero y crear celdas
+    for(let row = 0; row < 4; row++)
+    {
+        for(let col = 0; col < 4; col++)
+        {
+            let value = board[row][col]; //lee e valor de esa posicion en el tablero. row=0, col=1 board[0][1].
+            let cell = document.createElement('div'); //creo  un div en memoria (no esta en HTML todavia)
+            cell.classList.add('grid-cell'); //pone la clase grid-cell (estilos)
+            if (value !== 0)
+            {
+                cell.textContent = value;
+                cell.classList.add('tile-' + value); //classList = ['grid-cell', 'tile-(value)'].
+            }
+            grid.appendChild(cell); //lo meto en el html
+        }
+    }
+}
+
+//creo grid y recorro todo asignando valor en cada posicion. Creo un div por cada posicion y le meto la clase grid
+//si el valor no es 0, le pongo el valor que corresponda y le doy color.
+
+
+function addRandomTile()
+{
+    let emptyCells = [];
+    for (let row = 0; row < 4; row++)
+    {
+        for (let col = 0; col < 4; col++)
+        {
+            if (board[row][col] === 0)
+                emptyCells.push({row: row, col: col});
+        }
+    }
+    //comprobar que celda esta vacia y pushearlo a emptycells para elegir una.
+    
+    console.log("Celdas vacías", emptyCells);
+    if (emptyCells.length === 0)
+        return;
+
+    //elegir celda random vacia y añadirle valor 2 o 4.
+    let randomIndex = Math.floor(Math.random() * emptyCells.length);
+    let randomCell = emptyCells[randomIndex];
+    console.log("celda elegida:", randomCell);
+    let value = Math.random()  < 0.9 ? 2 : 4; // si value < 0.9 pone 2, si no 4 (90% posibilidades 2)
+    board[randomCell.row][randomCell.col] = value;
+    console.log("Se añadio un ", value, "en poscioin", randomCell);
+}
